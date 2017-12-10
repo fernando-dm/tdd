45aa712570bf4909c29acf85ea947570f7c3f8c0
Kolo kata <kolo@yo.com>
2017-12-10 16:29:08 -0300

    16 - depreco integerValue por numerator, siguiendo los 3 pasos del proceso

--- math.fraction/src/main/java/math/fraction/Fraction.java
+++ math.fraction/src/main/java/math/fraction/Fraction.java
@@ -4,15 +4,17 @@ import static java.lang.Integer.max;
 
 public class Fraction {
     private final int integerValue;
-    private int numerator;
+    private final int numerator;
     private final int denominator;
 
     public Fraction(int integerValue) {
         this.integerValue = integerValue;
+        this.numerator = integerValue;
         this.denominator =1;
     }
     public Fraction(int numerator, int denominator) {
         this.integerValue = numerator;
+        this.numerator = numerator;
         this.denominator = denominator;
     }
 
@@ -24,28 +26,28 @@ public class Fraction {
                 ((otherFraction.denominator%this.denominator) ==0) ){
             deno = max(otherFraction.denominator, this.denominator);
 
-            fraction = new Fraction((deno/this.denominator*this.integerValue) +
-                    otherFraction.integerValue,deno);
+            fraction = new Fraction((deno/this.denominator*this.numerator) +
+                    otherFraction.numerator,deno);
 
         } else if(otherFraction.denominator !=0 && (otherFraction.denominator%this.denominator) !=0){
             deno = (otherFraction.denominator * this.denominator);
-            fraction = new Fraction((deno/this.denominator*this.integerValue) +
-                    deno/otherFraction.denominator*otherFraction.integerValue,deno);
+            fraction = new Fraction((deno/this.denominator*this.numerator) +
+                    deno/otherFraction.denominator*otherFraction.numerator,deno);
         }
         else{
-            fraction =  new Fraction(this.integerValue +
-                    otherFraction.integerValue,denominator);
+            fraction =  new Fraction(this.numerator +
+                    otherFraction.numerator,denominator);
         }
 
         return fraction;
     }
 
     public int intValue() {
-        return integerValue;
+        return numerator;
     }
 
     public int getNumerator() {
-        return integerValue;
+        return numerator;
     }
 
     public int getDenominator() {
