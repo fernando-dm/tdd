f5a032cbd841f23ae35c4a755c6f022780f16b32
Kolo kata <kolo@yo.com>
2017-12-24 16:11:33 -0300

    Kotlin COMMIT 2: Agrego test basicos

--- math.fraction/src/main/kotlin/Fraction.kt
+++ math.fraction/src/main/kotlin/Fraction.kt
@@ -1,15 +1,40 @@
-class Fraction{
+import java.lang.Integer.max
 
-    private var integerValue: Int
+class Fraction{
+    val integerValue: Int
+    private var denominator=0 // una val no puede ser reasignada,
+                             // var si (es mutable)
 
     constructor(integerValue: Int){
-     this.integerValue = integerValue
+        this.integerValue = integerValue
     }
+
+    constructor(integerValue: Int, denominator: Int){
+        this.integerValue = integerValue
+        this.denominator = denominator
+    }
+
     fun plus(otherfraction: Fraction):Fraction{
+
+        if ((otherfraction.denominator%this.denominator)==0){
+            val deno = max(otherfraction.denominator,this.denominator)
+            return (Fraction((this.integerValue*(deno/this.denominator))+
+                    otherfraction.integerValue,deno))
+        }
+
         return Fraction(this.integerValue +
-                otherfraction.integerValue)
+                otherfraction.integerValue, denominator)
     }
-    fun toInte():Int{
+
+    fun toIntValue():Int{
         return this.integerValue
+    }
+
+    fun getNumerator(): Int {
+        return this.integerValue
+    }
+
+    fun getDenominator(): Int {
+        return denominator
     }
 }
--- math.fraction/src/test/kotlin/FractionTest.kt
+++ math.fraction/src/test/kotlin/FractionTest.kt
@@ -7,8 +7,28 @@ class TestFraction {
     fun zeroPlusZeroReturnZero() {
         val sum: Fraction = Fraction(0).
                 plus(Fraction(0))
-        println(sum.toInte())
-        assertEquals(0,sum.toInte())
+        println(sum.toIntValue())
+        assertEquals(0,sum.toIntValue())
+    }
+    @Test
+    fun addIntPlusIntReturnSum(){
+        val sum: Fraction = Fraction(3, 0).
+                plus(Fraction(3, 0))
+        assertEquals(6,sum.toIntValue())
+    }
+    @Test
+    fun sumNumeratorGivenSameDenominator(){
+        val sum: Fraction = Fraction(2,3).
+                plus(Fraction(1,3))
+        assertEquals(3,sum.getNumerator())
+    }
+
+    @Test
+    fun sumNumeratorGivenDifferentMultipleDenominator(){
+        val sum : Fraction = Fraction(3,2).
+                plus(Fraction(6,4))
+        assertEquals(12,sum.getNumerator())
+        assertEquals(4,sum.getDenominator())
     }
 
 
